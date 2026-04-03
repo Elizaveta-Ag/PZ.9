89fee7c2e0805334264cebc1306335889b28e0c6
Elizaveta-Ag <elizavetaageeva882@gmail.com>
2026-04-03 13:06:10 +0300

    Create пз9_2.cpp

---
+++ пз9_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int n;
+    cin >> n;
+
+    switch(n){
+        case 1:
+            cout << "Понедельник"; break;
+         case 2:
+            cout << "Вторник"; break;
+         case 3:
+            cout << "Среда"; break;
+         case 4:
+            cout << "Четверг"; break;
+         case 5:
+            cout << "Пятница"; break;
+         case 6:
+            cout << "Суббота"; break;
+         case 7:
+            cout << "Воскресенье"; break;
+        default:
+            cout << "Неверный день"; break;
+    }
+
+
+    return 0;
+}
