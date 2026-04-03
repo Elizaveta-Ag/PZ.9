19c9b24e20bc6cd029d5848843fa6ad2309ac56c
Elizaveta-Ag <elizavetaageeva882@gmail.com>
2026-04-03 13:08:04 +0300

    Create пз9_6.cpp

---
+++ пз9_6.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int n;
+    cin >> n;
+
+    switch(n){
+        case 0:
+            cout << "Ноль"; break;
+        case 1:
+            cout << "Один"; break;
+        case 2:
+            cout << "Два"; break;
+        case 3:
+            cout << "Три"; break;
+        case 4:
+            cout << "Четыре"; break;
+        case 5:
+            cout << "Пять"; break;
+        case 6:
+            cout << "Шесть"; break;
+        case 7:
+            cout << "Семь"; break;
+        case 8:
+            cout << "Восемь"; break;
+        case 9:
+            cout << "Девять"; break;        
+        default:
+            cout << "Ошибка"; break;    
+    }
+
+
+    return 0;
+}
